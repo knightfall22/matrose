cb5fa9876f76d0d88179c872bab534b738a2432c
knightfall22 <pelumi066@gmail.com>
2026-02-25 13:05:40 +0200

    integrate task with docker(moby) SDK

--- main.go
+++ main.go
@@ -2,68 +2,61 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
-	"github.com/golang-collections/collections/queue"
-	"github.com/google/uuid"
-	"github.com/knightfall22/matrose/manager"
-	"github.com/knightfall22/matrose/node"
 	"github.com/knightfall22/matrose/task"
-	"github.com/knightfall22/matrose/worker"
+	"github.com/moby/moby/client"
 )
 
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:13",
+		ENV: []string{
+			"POSTGRES_USER=matty",
+			"POSTGRES_PASSWORD=sailor",
+		},
+	}
+
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
+	}
+
+	fmt.Printf(
+		"Container %s is running with config %v\n", result.ContainerID, c)
+	return &d, &result
+
+}
+
+func containerStop(d *task.Docker, id string) *task.DockerResult {
+	res := d.Stop(id)
+	if res.Error != nil {
+		fmt.Printf("%v\n", res.Error)
+		return nil
+	}
+
+	fmt.Printf(
+		"Container %s has been stopped and removed\n", res.ContainerID)
+	return &res
+}
 func main() {
-	t := task.Task{
-		ID:     uuid.New(),
-		Name:   "Task-1",
-		State:  task.Pending,
-		Image:  "Image-1",
-		Memory: 1024,
-		Disk:   1,
+	fmt.Printf("create a test container\n")
+	dockerTask, createResult := createContainer()
+	if createResult.Error != nil {
+		fmt.Printf("%v", createResult.Error)
+		os.Exit(1)
 	}
 
-	te := task.TaskEvent{
-		ID:        uuid.New(),
-		State:     task.Pending,
-		Timestamp: time.Now(),
-		Task:      t,
-	}
-
-	fmt.Printf("task: %v\n", t)
-	fmt.Printf("task event: %v\n", te)
-
-	w := worker.Worker{
-		Name:  "Worker-1",
-		Queue: *queue.New(),
-		Db:    make(map[uuid.UUID]*task.Task),
-	}
-
-	fmt.Printf("worker: %v/n", w)
-	w.CollectStats()
-	w.RunTask()
-	w.StartTask()
-	w.StopTask()
-
-	m := manager.Manager{
-		Pending: *queue.New(),
-		TaskDb:  make(map[string][]*task.Task),
-		EventDb: make(map[string][]*task.TaskEvent),
-		Workers: []string{"Worker-1"},
-	}
-
-	fmt.Printf("manager: %v\n", m)
-	m.UpdateTasks()
-	m.SelectWorker()
-	w.CollectStats()
-
-	n := node.Node{
-		Name:   "Node-1",
-		Ip:     "192.168.1.1",
-		Cores:  4,
-		Memory: 1024,
-		Disk:   25,
-		Role:   "worker",
-	}
-
-	fmt.Printf("node: %v\n", n)
+	time.Sleep(time.Second * 5)
+	fmt.Printf("stopping container %s\n", createResult.ContainerID)
+	_ = containerStop(dockerTask, createResult.ContainerID)
 }
--- task/task.go
+++ task/task.go
@@ -1,10 +1,19 @@
 package task
 
 import (
+	"context"
+	"io"
+	"log"
+	"math"
+	"os"
 	"time"
 
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
+	"github.com/moby/moby/client"
+	"github.com/moby/moby/pkg/stdcopy"
 )
 
 //Task is the smallest unit of work in an orchestration system and typically runs a container.
@@ -30,9 +39,11 @@ type Task struct {
 	State State
 	Image string
 
+	CPU float64
+
 	// Memory and Disk will help the system identify the number of resources a task needs.
-	Memory int
-	Disk   int
+	Memory int64
+	Disk   int64
 
 	// `ExposedPorts` and `PortBindings` are used by
 	// Docker to ensure the machine allocates the proper network ports for
@@ -51,4 +62,162 @@ type TaskEvent struct {
 	State     State
 	Timestamp time.Time
 	Task      Task
+}
+
+func NewConfig(t *Task) *Config {
+	return &Config{
+		Name:          t.Name,
+		CPU:           t.CPU,
+		Memory:        t.Memory,
+		Disk:          t.Disk,
+		ExposedPorts:  t.ExposedPorts,
+		Image:         t.Image,
+		RestartPolicy: t.RestartPolicy,
+	}
+}
+
+// Contains information about the configuration of the task
+type Config struct {
+	// Name of the task. Used to identify the task,
+	// also the name of the running container
+	Name         string
+	AttachStdin  bool
+	AttachStdout bool
+	AttachStderr bool
+
+	ExposedPorts nat.PortSet
+	Cmd          []string
+
+	//Holds the name of the image to run
+	Image string
+
+	//The scheduler will use `Memory`, `Disk`, `CPU` to find a node in
+	// the cluster capable of running a task.
+	// They will also be used to tell the Docker daemon the number of
+	// resources a task requires.
+	CPU    float64
+	Memory int64
+	Disk   int64
+
+	// Allows a user to specify environment variables that
+	// will get passed into the container.
+	ENV []string
+
+	// RestartPolicy field tells the Docker daemon what to
+	// do if a container dies unexpectedly.Can be either "",
+	// `always`, `unless-stopped`, or `on-failure`. Setting this
+	// field to always will, as its name implies, restart a container if it
+	// stops. Setting it to unless-stopped will restart a container
+	// unless it has been stopped (e.g., by docker stop). Setting it to
+	// on-failure will restart the container if it exits due to an error
+	// (i.e., a nonzero exit code).
+	RestartPolicy string
+}
+
+type DockerResult struct {
+	Error error
+	//TODO: Convert action into an enum
+	Action      string
+	ContainerID string
+	Result      string
+}
+
+// Encapsulates everything we need to run our task as a Docker container.
+type Docker struct {
+	Client *client.Client
+	Config Config
+}
+
+func (d *Docker) Run() DockerResult {
+	ctx := context.Background()
+	reader, err := d.Client.ImagePull(
+		ctx, d.Config.Image, image.PullOptions{},
+	)
+	if err != nil {
+		log.Printf("Error pulling image %s: %v\n", err)
+		return DockerResult{Error: err}
+	}
+
+	io.Copy(os.Stdout, reader)
+
+	rp := container.RestartPolicy{
+		Name: container.RestartPolicyMode(d.Config.RestartPolicy),
+	}
+
+	r := container.Resources{
+		Memory:   d.Config.Memory,
+		NanoCPUs: int64(d.Config.CPU * math.Pow(10, 9)),
+	}
+
+	cc := container.Config{
+		Image: d.Config.Image,
+		Tty:   false,
+		Env:   d.Config.ENV,
+		//Debug: Watch for potential errors
+		ExposedPorts: d.Config.ExposedPorts,
+	}
+
+	hc := container.HostConfig{
+		RestartPolicy: rp,
+		Resources:     r,
+	}
+	resp, err := d.Client.ContainerCreate(
+		ctx,
+		&cc,
+		&hc,
+		nil,
+		nil,
+		d.Config.Name,
+	)
+	if err != nil {
+		log.Printf("Error creating container using image %s: %v\n", d.Config.Image, err)
+		return DockerResult{Error: err}
+	}
+
+	err = d.Client.ContainerStart(ctx, resp.ID, container.StartOptions{})
+	if err != nil {
+		log.Printf("Error starting container %s: %v\n", resp.ID, err)
+		return DockerResult{Error: err}
+	}
+
+	// d.Config.Runtime.ContainerID = resp.ID
+
+	out, err := d.Client.ContainerLogs(ctx, resp.ID, container.LogsOptions{
+		ShowStdout: true,
+		ShowStderr: true,
+	})
+	if err != nil {
+		log.Printf("Error getting logs for container %s: %v\n", resp.ID, err)
+		return DockerResult{Error: err}
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+	return DockerResult{
+		ContainerID: resp.ID,
+		Action:      "start",
+		Result:      "success",
+	}
+}
+
+func (d *Docker) Stop(id string) DockerResult {
+	log.Printf("Attempting to stop container %v", id)
+	ctx := context.Background()
+
+	err := d.Client.ContainerStop(ctx, id, container.StopOptions{})
+	if err != nil {
+		log.Printf("Error stopping container %s: %v\n", id, err)
+		return DockerResult{Error: err}
+	}
+
+	err = d.Client.ContainerRemove(ctx, id, container.RemoveOptions{
+		RemoveVolumes: true,
+		RemoveLinks:   false,
+		Force:         false,
+	})
+	if err != nil {
+		log.Printf("Error removing container %s: %v\n", id, err)
+		return DockerResult{Error: err}
+	}
+
+	return DockerResult{Action: "stop", Error: nil, Result: "success"}
 }
